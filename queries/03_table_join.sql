7338e465b2f0fa2073f179937810efa9187263a1
yui32397 <yui352574@gmail.com>
2026-07-06 04:55:29 +0900

    Update 03_table_join.sql

--- queries/03_table_join.sql
+++ queries/03_table_join.sql
@@ -1,1 +1,18 @@
+-- ==============================================================================
+-- レベル3: 複数テーブルの結合（LEFT JOIN）
+-- 【ビジネス要求】A/Bテストの配信・行動ログに対して会員マスターを結合し、年代別のベーステーブルを作成
+-- ==============================================================================
+
+SELECT 
+    log.user_id,
+    log.group,
+    log.converted,
+    usr.age_group,
+    usr.gender
+FROM 
+    sbux_ab_test_results AS log
+LEFT JOIN 
+    sbux_user_master AS usr
+ON 
+    log.user_id = usr.user_id;
 
